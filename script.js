aa830350ef7fbdabc330b99917bbdfb09b984fb9
RahmatRamadhan17 <rahmat.ramadhan0217@gmail.com>
2025-11-05 15:22:05 +0700

    Update Multi Bahasa

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Website Rumah Batik Bintan aktif!");
+function setLanguage(lang) {
+  localStorage.setItem("siteLang", lang);
+
+  // Loop semua element yg punya data-key
+  document.querySelectorAll("[data-key]").forEach(element => {
+    const key = element.getAttribute("data-key");
+    if (translations[lang][key]) {
+      element.innerText = translations[lang][key];
+    }
+  });
+}
+
+window.addEventListener("load", () => {
+  const saved = localStorage.getItem("siteLang") || "id";
+  document.getElementById("languageSwitcher").value = saved;
+  setLanguage(saved);
+});
+
+document.getElementById("languageSwitcher").addEventListener("change", e => {
+  setLanguage(e.target.value);
+});
